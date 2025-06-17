020a0243896ced0fb38d8c37187e308031003523
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:59 +0300

    CompositeHelpers.cs

---
+++ maybe-plugins/Composition/CompositeHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FluentOptionals.Composition
+{
+    internal static class CompositeHelpers
+    {
+        private static readonly Func<bool> False = () => false;
+        
+        public static bool IsSome<T>(this Optional<T> optional)
+        {
+            return optional.Match(_ => true, False);
+        }
+
+        public static T ValueOrDefault<T>(this Optional<T> optional)
+        {
+            return optional.ValueOr(default(T));
+        }
+    }
+}
