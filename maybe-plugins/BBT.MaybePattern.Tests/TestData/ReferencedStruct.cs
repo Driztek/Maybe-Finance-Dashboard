d3a743f42346307a50a3bfb0129d8114b886f781
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:52 +0300

    ReferencedStruct.cs

---
+++ maybe-plugins/BBT.MaybePattern.Tests/TestData/ReferencedStruct.cs
@@ -0,0 +1,18 @@
+﻿namespace BBT.MaybePattern.Tests.TestData
+{
+    /// <summary>
+    /// Used for test purposes.
+    /// </summary>
+    public struct ReferencedStruct
+    {
+        /// <summary>
+        /// Gets or sets the referencing struct.
+        /// </summary>
+        public BaseStruct ReferencingStruct { get; set; }
+
+        /// <summary>
+        /// Gets or sets the referencing class.
+        /// </summary>
+        public BaseClass ReferencingClass { get; set; }
+    }
+}
