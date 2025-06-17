0b2ae791fb94db3889cd68d004db47b09f76ab0d
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:10:50 +0300

    ReferencedClass.cs

---
+++ maybe-plugins/BBT.MaybePattern.Tests/TestData/ReferencedClass.cs
@@ -0,0 +1,18 @@
+﻿namespace BBT.MaybePattern.Tests.TestData
+{
+    /// <summary>
+    /// Used for test purposes.
+    /// </summary>
+    public class ReferencedClass
+    {
+        /// <summary>
+        /// Gets or sets the referencing class.
+        /// </summary>
+        public BaseClass ReferencingClass { get; set; }
+
+        /// <summary>
+        /// Gets or sets the referencing struct.
+        /// </summary>
+        public BaseStruct ReferencingStruct { get; set; }
+    }
+}
